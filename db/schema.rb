9762a3d1051a793a6088fc5a0d915198f4f19656
daniel-ishitani <ishitanidim@gmail.com>
2019-08-17 16:01:28 -0300

    student course association

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_17_153507) do
+ActiveRecord::Schema.define(version: 2019_08_17_175158) do
 
   create_table "courses", force: :cascade do |t|
     t.string "code"
     t.string "name"
     t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "student_courses", force: :cascade do |t|
+    t.integer "student_id"
+    t.integer "course_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
